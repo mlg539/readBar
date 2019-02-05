1aa2c944e9288373dad2744e8ec0a01909a5bedb
“r.email <pjhemo@outlook.com>
2019-02-05 16:20:58 +0800

    添加了enter键

--- readBar-server/app.js
+++ readBar-server/app.js
@@ -1,5 +1,6 @@
 //使用express创建服务器
 const express=require("express")
+const fs=require("fs")
 var app=express();
 app.listen(3000,()=>{
 	console.log("服务器创建成功")
@@ -11,13 +12,16 @@ app.use(express.static("public"))
 const cors = require("cors");
 //2:配置cors
 app.use(cors({
-	origin: ["http://127.0.0.1:3000",
-		"http://localhost:3000"],//允许列表
+	origin: ["http://127.0.0.1:5500",
+		"http://localhost:5500"],//允许列表
 	credentials: true   //是否验证
 }))
 
 app.get("/getlist",(req,res)=>{
-	res.send("你好")
+	fs.readdir("./public",(err,data)=>{
+		res.send({code:1,msg:data})
+	})
+
 })
 
 
