941365eaf7f41401d91ec7a0ee4e82cae9140cf7
“r.email <pjhemo@outlook.com>
2019-02-05 15:21:56 +0800

    version1

--- readBar-server/app.js
+++ readBar-server/app.js
@@ -6,6 +6,20 @@ app.listen(3000,()=>{
 })
 app.use(express.static("public"))
 
+//跨域访问配置
+//1:加载模块cors
+const cors = require("cors");
+//2:配置cors
+app.use(cors({
+	origin: ["http://127.0.0.1:3000",
+		"http://localhost:3000"],//允许列表
+	credentials: true   //是否验证
+}))
+
+app.get("/getlist",(req,res)=>{
+	res.send("你好")
+})
+
 
 
 
